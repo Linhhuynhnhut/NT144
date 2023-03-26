acf4a3b94ad20dfa68be4d622653315f18bdb540
Nhut Linh <linhhn1210@gmail.com>
2023-03-26 15:58:56 +0700

    design intro screen

--- src/screens/IntroScreen.js
+++ src/screens/IntroScreen.js
@@ -1,41 +1,75 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Pressable, Image } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Pressable,
+  Image,
+  FlatList,
+  Dimensions,
+} from "react-native";
 import Carousel from "react-native-snap-carousel";
 import React, { useState, useEffect } from "react";
 import img1 from "../../assets/image/food0.png";
 import img2 from "../../assets/image/food1.png";
 import img3 from "../../assets/image/food2.png";
-const IMAGES = {
-  "food0.png": require("../../assets/image/food0.png"),
-  "food1.png": require("../../assets/image/food1.png"),
-  "food2.png": require("../../assets/image/food2.png"),
-};
 
+const { width, height } = Dimensions.get("window");
+
+const slides = [
+  {
+    index: 0,
+    image: require("../../assets/image/food0.png"),
+  },
+  {
+    index: 1,
+    image: require("../../assets/image/food1.png"),
+  },
+  {
+    index: 2,
+    image: require("../../assets/image/food2.png"),
+  },
+];
 const Item = ({ item }) => {
-  return <Image source={item ? item : ""} style={styles.imageCard} />;
+  return <Image source={item.image} style={styles.imageCard} />;
 };
 
 const IntroScreen = ({ navigation, route }) => {
-  const [cardArrState, setCardArrState] = useState([img1, img2, img3]);
+  const [currentSlideIndex, setCurrentSlideIndex] = React.useState(0);
+  const updateCurrentSlideIndex = (e) => {
+    const contentOffsetX = e.nativeEvent.contentOffset.x;
+    const currentIndex = Math.round(contentOffsetX / width);
+    setCurrentSlideIndex(currentIndex);
+  };
 
   return (
     <>
       <View style={styles.introView}>
         <View style={styles.imageView}>
-          <Carousel
-            layout={"default"}
-            //layoutCardOffset={18}
-            data={cardArrState}
+          <FlatList
+            onMomentumScrollEnd={updateCurrentSlideIndex}
+            pagingEnabled
+            data={slides}
             renderItem={({ item }) => <Item item={item} />}
-            sliderWidth={300}
-            itemWidth={300}
-            style={{ backgroundColor: "#fff" }}
+            //contentContainerStyle={{ height: height * 0.75 }}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            //style={{ backgroundColor: "#fff" }}
           />
         </View>
         <View style={styles.indicatorContainer}>
-          <View style={styles.currentIndicator}></View>
-          <View style={styles.indicator}></View>
-          <View style={styles.indicator}></View>
+          {slides.map((_, index) => (
+            <View
+              key={index}
+              style={[
+                styles.indicator,
+                currentSlideIndex == index && {
+                  backgroundColor: "#f27e35",
+                  width: 25,
+                },
+              ]}
+            />
+          ))}
         </View>
         <Text style={styles.title}>Delicious Food!</Text>
         <Text style={styles.discription}>
@@ -88,7 +122,6 @@ const styles = StyleSheet.create({
   indicatorContainer: {
     height: 50,
     justifyContent: "center",
-    alignItems: "center",
     flexDirection: "row",
   },
   currentIndicator: {
@@ -115,11 +148,12 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     shadowColor: "#000",
     shadowOffset: {
-      width: 0,
-      height: 2,
+      width: 5,
+      height: 5,
     },
-    shadowOpacity: 0.25,
+    shadowOpacity: 0.8,
     shadowRadius: 4,
+    elevation: 5,
   },
 });
 export default IntroScreen;
