0eca5cbc91e3873d1150b5dabdbee50cae3af1e6
Lý Quốc Kiệt <102638114+Lyquockiet2002@users.noreply.github.com>
2023-04-02 17:14:56 +0700

    HomeScreen test

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,36 +1,217 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Pressable } from "react-native";
+import React from 'react';
+import {
+  Dimensions,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  ScrollView,
+  TextInput,
+  TouchableHighlight,
+  TouchableOpacity
+} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import COLORS from '../../consts/colors';
+import categories from '../../consts/categories';
+import foods from '../../consts/foods';
+const {width} = Dimensions.get("screen");
+const cardWidth = width / 2 - 20;
 
-const HomeScreen = ({ navigation }) => {
+const HomeScreen = ({navigation}) => {
+  const [selectedCategoryIndex, setSelectedCategoryIndex] = React.useState(0);
+
+  const ListCategories = () => {
+    return (
+      <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={style.categoriesListContainer}>
+        {categories.map((category, index) => (
+          <TouchableOpacity
+            key={index}
+            activeOpacity={0.8}
+            onPress={() => setSelectedCategoryIndex(index)}>
+            <View
+              style={{
+                backgroundColor:
+                  selectedCategoryIndex == index
+                    ? COLORS.primary
+                    : COLORS.secondary,
+                ...style.categoryBtn,
+              }}>
+              <View style={style.categoryBtnImgCon}>
+                <Image
+                  source={category.image}
+                  style={{height: 45, width: 35, resizeMode: 'cover'}}
+                />
+              </View>
+              <Text
+                style={{
+                  fontSize: 15,
+                  fontWeight: 'bold',
+                  marginLeft: 5,
+                  color:
+                    selectedCategoryIndex == index
+                      ? COLORS.white
+                      : COLORS.primary,
+                }}>
+                {category.name}
+              </Text>
+            </View>
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
+    );
+  };
+  const Card = ({food}) => {
+    return (
+      <TouchableHighlight
+        underlayColor={COLORS.white}
+        activeOpacity={0.9}
+        onPress={() => navigation.navigate('DetailScreen', food)}>
+        <View style={style.card}>
+          <View style={{alignItems: 'center', top: -20}}>
+            <Image source={food.image} style={{height: 120, width: 120}} />
+          </View>
+          <View style={{marginHorizontal: 20}}>
+            <Text style={{fontSize: 16, fontWeight: 'bold'}}>{food.name}</Text>
+            <Text style={{fontSize: 16, color: COLORS.grey, marginTop: 2}}>
+              {food.ingredients}
+            </Text>
+          </View>
+          <View
+            style={{
+              marginTop: 10,
+              marginHorizontal: 20,
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+            }}>
+            <Text style={{fontSize: 14, fontWeight: 'bold'}}>
+              Time:
+            </Text>
+            <View style={style.addToCartBtn}>
+              <Icon name="add" size={20} color={COLORS.white} />
+            </View>
+          </View>
+        </View>
+      </TouchableHighlight>
+    );
+  };
   return (
-    <>
-      <View style={styles.introView}>
-        <Text>Home Screen</Text>
-        <Pressable
-          style={styles.button}
-          onPress={() => navigation.navigate("Search", {})}
-        >
-          <Text>Button</Text>
-        </Pressable>
+    <SafeAreaView style={{flex: 1, backgroundColor: COLORS.white}}>
+      <View style={style.header}>
+        <View>
+          <View style={{flexDirection: 'row'}}>
+            <Text style={{fontSize: 28}}>Hello,</Text>
+            <Text style={{fontSize: 28, fontWeight: 'bold', marginLeft: 10}}>
+              Chef
+            </Text>
+          </View>
+          <Text style={{marginTop: 5, fontSize: 22, color: COLORS.grey}}>
+          What do you want to cook ?
+          </Text>
+        </View>
+        <Image
+          source={require('../../assets/person.png')}
+          style={{height: 50, width: 60, borderRadius: 25}}
+        />
       </View>
-    </>
+      <View
+        style={{
+          marginTop: 40,
+          flexDirection: 'row',
+          paddingHorizontal: 20,
+        }}>
+        <View style={style.inputContainer}>
+          <Icon name="search" size={28} />
+          <TextInput
+            style={{flex: 1, fontSize: 18}}
+            placeholder="Search for food"
+          />
+        </View>
+        <View style={style.sortBtn}>
+          <Icon name="tune" size={28} color={COLORS.white} />
+        </View>
+      </View>
+      <View>
+        <ListCategories />
+      </View>
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        numColumns={2}
+        data={foods}
+        renderItem={({item}) => <Card food={item} />}
+      />
+    </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({
-  introView: {
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#f0dba3",
-    width: "100%",
-    height: "100%",
+const style = StyleSheet.create({
+  header: {
+    marginTop: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
   },
-  button: {
-    backgroundColor: "#fff",
-    width: 100,
+  inputContainer: {
+    flex: 1,
+    height: 50,
+    borderRadius: 10,
+    flexDirection: 'row',
+    backgroundColor: COLORS.light,
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+  sortBtn: {
+    width: 50,
+    height: 50,
+    marginLeft: 10,
+    backgroundColor: COLORS.primary,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  categoriesListContainer: {
+    paddingVertical: 30,
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+  categoryBtn: {
+    height: 45,
+    width: 120,
+    marginRight: 7,
+    borderRadius: 30,
+    alignItems: 'center',
+    paddingHorizontal: 5,
+    flexDirection: 'row',
+  },
+  categoryBtnImgCon: {
+    height: 35,
+    width: 35,
+    backgroundColor: COLORS.white,
+    borderRadius: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  card: {
+    height: 220,
+    width: cardWidth,
+    marginHorizontal: 10,
+    marginBottom: 20,
+    marginTop: 50,
+    borderRadius: 15,
+    elevation: 13,
+    backgroundColor: COLORS.white,
+  },
+  addToCartBtn: {
     height: 30,
-    alignItems: "center",
-    justifyContent: "center",
+    width: 30,
+    borderRadius: 20,
+    backgroundColor: COLORS.primary,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 export default HomeScreen;
