aabd407c57abdfe6da1f0f190730dbc2fc040ff3
Lý Quốc Kiệt <102638114+Lyquockiet2002@users.noreply.github.com>
2023-04-02 17:01:38 +0700

    add consts/categories

---
+++ consts/categories.js
@@ -0,0 +1,9 @@
+const categories = [
+    {id: '1', name: 'Breakfast', image: require('../assets/categories/burger.png')},
+    {id: '2', name: '  Lunch', image: require('../assets/categories/sushi.png')},
+    {id: '3', name: '  Dinner', image: require('../assets/categories/pizza.png')},
+    {id: '4', name: ' Another', image: require('../assets/categories/salad.png')},
+  ];
+  
+  export default categories;
+  
