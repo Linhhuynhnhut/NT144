587699e03706637d267381cf8d308cb55738e2d4
Lý Quốc Kiệt <102638114+Lyquockiet2002@users.noreply.github.com>
2023-04-06 16:27:07 +0700

    move food picture to assets/foods

--- consts/foods.js
+++ consts/foods.js
@@ -6,28 +6,28 @@ const foods = [
     name: 'Meat Pizza',
     ingredients: 'Mixed Pizza',
     price: '8.30',
-    image: require('../assets/meatPizza.png'),
+    image: require('../assets/foods/meatPizza.png'),
   },
   {
     id: '2',
     name: 'Cheese Pizza',
     ingredients: 'Cheese Pizza',
     price: '7.10',
-    image: require('../assets/cheesePizza.png'),
+    image: require('../assets/foods/cheesePizza.png'),
   },
   {
     id: '3',
     name: 'Chicken Burger',
     ingredients: 'Fried Chicken',
     price: '5.10',
-    image: require('../assets/chickenBurger.png'),
+    image: require('../assets/foods/chickenBurger.png'),
   },
   {
     id: '4',
     name: 'Sushi Makizushi',
     ingredients: 'Salmon Meat',
     price: '9.55',
-    image: require('../assets/sushiMakizushi.png'),
+    image: require('../assets/foods/sushiMakizushi.png'),
   },
 ];
 
